1a68fb0cfb15c0771f5a92e65db999cb5ab22fbf
readyforread <arm2000arms@gmail.com>
2025-09-20 19:34:38 +0300

    Create warp_generator.sh

---
+++ warp_generator.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Генератор конфигурации WireGuard для Cloudflare WARP
+# Сохраняет конфиг в warp.conf
+
+set -e
+
+# Генерация ключей
+priv="${1:-$(wg genkey)}"
+pub="${2:-$(echo "${priv}" | wg pubkey)}"
+
+# API Cloudflare WARP
+api="https://api.cloudflareclient.com/v0i1909051800"
+
+# Функции для запросов
+ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
+sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
+
+# Регистрация устройства
+response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
+
+id=$(echo "$response" | jq -r '.result.id')
+token=$(echo "$response" | jq -r '.result.token')
+
+# Включаем WARP
+response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
+
+peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
+client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
+client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
+
+# Собираем конфиг
+conf=$(cat <<-EOM
+[Interface]
+PrivateKey = ${priv}
+S1 = 0
+S2 = 0
+Jc = 120
+Jmin = 23
+Jmax = 911
+H1 = 1
+H2 = 2
+H3 = 3
+H4 = 4
+MTU = 1280
+Address = ${client_ipv4}, ${client_ipv6}
+DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
+
+[Peer]
+PublicKey = ${peer_pub}
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = 162.159.192.1:500
+EOM
+)
+
+# Сохраняем конфиг
+echo "$conf" > warp.conf
+echo "✅ Конфиг сохранён в warp.conf"
